e7c122672c89b91e9a4d7042bf82f6848acbba54
Frehiwot-G <frehiwotgem@gmail.com>
2024-06-08 11:51:04 +0300

    final design

--- src/index.js
+++ src/index.js
@@ -1,31 +1,105 @@
-function displaytemp(response){
-    let temp= document.querySelector(".temp");
-    temp.innerHTML = Math.round(response.data.temperature.current);
-    console.log(Math.round(response.data.temperature.current));
+let currCity = "London";
+let units = "metric";
+
+// Selectors
+let city = document.querySelector(".weather__city");
+let currentDateELement = document.querySelector(".weather__datetime ");
+let weather__forecast = document.querySelector(".weather__forecast");
+let weather__temperature = document.querySelector(".weather__temperature");
+let weather__icon = document.querySelector(".weather__icon");
+let weather__realfeel = document.querySelector(".weather__realfeel");
+let weather__humidity = document.querySelector(".weather__humidity");
+let weather__wind = document.querySelector(".weather__wind");
+let weather__pressure = document.querySelector(".weather__pressure");
+
+// search
+document.querySelector(".weather__search").addEventListener("submit", (e) => {
+  let search = document.querySelector(".weather__searchform");
+  // prevent default action
+  e.preventDefault();
+  // change current city
+  currCity = search.value;
+  console.log(currCity);
+  // get weather forecast
+  getWeather();
+  // clear form
+  search.value = "";
+});
+
+// units
+document
+  .querySelector(".weather_unit_celsius")
+  .addEventListener("click", () => {
+    if (units !== "metric") {
+      // change to metric
+      units = "metric";
+      // get weather forecast
+      getWeather();
+    }
+  });
+
+document
+  .querySelector(".weather_unit_farenheit")
+  .addEventListener("click", () => {
+    if (units !== "imperial") {
+      // change to imperial
+      units = "imperial";
+      // get weather forecast
+      getWeather();
+    }
+  });
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
- 
-// let city  = document.querySelector("city"); 
-function displaycity(event){
-    event.preventDefault();
-    let h1 = document.querySelector("h1");
-    let city = document.querySelector("#city");
-    // let city = inputcity.value;
-    console.log(city.value); 
-    h1.innerHTML = city.value;
-    // let city = "Addis Ababa"
 
-    let apiKey = "e84352t57bcfd38870b8f1603bcaa4o4";
+let currentDate = new Date();
 
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+currentDateELement.innerHTML = formatDate(currentDate);
 
-    axios.get(apiUrl).then(displaytemp)
+function getWeather() {
+  const API_KEY = "e84352t57bcfd38870b8f1603bcaa4o4";
+  //   `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+
+  fetch(
+    `https://api.shecodes.io/weather/v1/current?query=${currCity}&key=${API_KEY}&units=${units}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      city.innerHTML = `${data.city}`;
+      weather__temperature.innerHTML = `${data.temperature.current.toFixed()}&#176`;
+      weather__icon.innerHTML = `   <img src=${data.condition.icon_url} />`;
+      weather__realfeel.innerHTML = `${data.temperature.feels_like.toFixed()}&#176`;
+      weather__humidity.innerHTML = `${data.temperature.humidity}%`;
+      weather__wind.innerHTML = `${data.wind.speed} ${
+        units === "imperial" ? "mph" : "m/s"
+      }`;
+      weather__pressure.innerHTML = `${data.temperature.pressure} hPa`;
+    });
 }
 
-let search = document.querySelector("#search");
-search.addEventListener("submit",displaycity);
-
-  
-// let city = "london";
-// APIKey = "5f4089afaf5945bdb6062636242904";
-
-// apiurl= "http://api.weatherapi.com/v1/current.json?query=${city}&key=${APIKey}";
+document.body.addEventListener("load", getWeather());
